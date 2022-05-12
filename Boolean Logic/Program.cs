d9bdc91276020c72b8b257f0f757aa3c5a914cb1
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-05-12 09:11:56 +0100

    Add files via upload

---
+++ Boolean Logic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age");
+            bool trueorfalse = 32 > 15;
+            Console.WriteLine(trueorfalse);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI violation?");
+            bool dui = 0 == 0;
+            Console.WriteLine(dui);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            bool ticket = 3 < 1;
+            Console.WriteLine(ticket);
+            Console.ReadLine();
+
+            Console.WriteLine("Qualified?");
+            bool qual = true && true ;
+            Console.WriteLine(qual);
+            Console.ReadLine();
+        }
+    }
+}
