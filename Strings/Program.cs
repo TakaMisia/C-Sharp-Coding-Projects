71ad91b8b735ff4fa8cdc62091302b873d34b2bf
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-05-18 14:52:50 +0100

    Add files via upload

---
+++ Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3 strings
+            string author = "Michalina Brodnicka";
+            string description = "I am young motivated person." +
+                "\n I am a mother also. \n \t" +
+                " my daughter name is Victoria.";
+
+            author = author.ToUpper();
+            Console.WriteLine(description);
+            //uppper letter 
+            Console.WriteLine(author);  
+            Console.ReadLine();
+
+           var sb = new System.Text.StringBuilder();   
+            sb.AppendLine("Piotr Kasiński");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+        }
+    }
+}
