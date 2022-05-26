8744aefff6a1fc1a57474a5275fecdee7e7c9473
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-05-26 11:24:57 +0100

    Add files via upload

---
+++ Exception Assignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Type a number :");
+            int typenum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type another  number :");
+            int typenum2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividiing that to number :");
+            int typenum3 = typenum1 / typenum2;
+            Console.WriteLine(typenum1 + " / " + typenum2 + " = " + typenum3);
+            Console.ReadLine();
+
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please write diffrent number because we are unable divding by zero");
+            return;
+        }
+        catch (FormatException ex )
+        {
+            Console.WriteLine("Write diffrent type");
+            return ;
+        }
+ 
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+    }
+}
