c03fb2c9240059e7b41ab483e1a589be59e2c190
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-05-31 20:08:25 +0100

    Add files via upload

---
+++ Method Assignment/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+
+        class answer
+        {
+                public int plus (int a)
+                {
+                    return a + 3;
+                }
+
+                public int minus (int a)
+                {
+                    return a - 4;
+                }
+
+                public int multi (int a)
+                {
+                    return a * 7;
+                }
+
+
+        }
+}
+
