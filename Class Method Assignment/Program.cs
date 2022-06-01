7ed22e3663cb783b93be2a3d0aee02027d9b682c
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-01 11:28:47 +0100

    Add files via upload

---
+++ Class Method Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose a number ?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            answer xyz = new answer();
+
+            Console.WriteLine("Your number dividedby 2  is " + xyz.divide(number));
+            Console.WriteLine("Your number minus 4  is " + xyz.minus(number));
+            Console.WriteLine("Your number times 7  is " + xyz.multi(number));
+            Console.ReadLine();
+        }
+    }
+}
