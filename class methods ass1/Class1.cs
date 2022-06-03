f4cef03a4e8ef4303068fe90c0cf12c77b3a8495
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-03 16:20:19 +0100

    Add files via upload

---
+++ class methods ass1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_methods_ass1
+{
+    class maths
+    {
+        public int multi(int x)
+        {
+            return x * 2;
+        }
+        //Create method to convert given decimal to interger.
+        public int deccon(decimal x)
+        {
+            return Convert.ToInt32(x);
+        }
+        //Create method to convert given string to interger.
+        public int stringcon(string x)
+        {
+            return Convert.ToInt32(x);
+        }
+    }
+}
---
+++ class methods ass1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_methods_ass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            maths obj1 = new maths();
+
+            Console.WriteLine("Please write number from  0 to 200");
+            int intsum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number time 2  is  " + obj1.multi(intsum));
+
+            Console.WriteLine("Please write decimal number to convert to an integer.");
+            decimal decsum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Your decimal as an interger is " + obj1.deccon(decsum));
+
+            Console.WriteLine("Please write string number to convert to an integer.");
+            string stringsum = Console.ReadLine();
+            Console.WriteLine("Your string as an interger is " + obj1.stringcon(stringsum));
+
+            Console.ReadLine();
+        }
+    }
+}
