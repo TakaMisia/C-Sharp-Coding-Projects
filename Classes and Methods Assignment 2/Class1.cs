6c6d75d471e1ab6a65321cdeb48f75f432db3c1b
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-04 14:47:24 +0100

    Add files via upload

---
+++ Classes and Methods Assignment 2/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment_2
+{
+    class math
+    {
+        public void Main()
+        {
+             int intnum1(int x)
+            {
+                return x;
+            }
+
+             int intnum2(int x)
+            {
+                return x;
+            }
+        }
+    }
+}
---
+++ Classes and Methods Assignment 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment_2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            math xyz  = new math ();
+
+            Console.WriteLine("Please write a number ");
+            int intnum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your number is : " + intnum1);
+
+            Console.WriteLine("Please write another number ");
+            int intnum2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your second  number is : " + intnum2);
+
+            int total = intnum1 + intnum2;
+            Console.WriteLine("Total result is " + total);
+            Console.ReadLine();
+        }
+    }
+
+}
