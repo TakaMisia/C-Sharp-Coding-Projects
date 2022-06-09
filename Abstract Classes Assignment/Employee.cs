c0a58843390f7e6e7f07a13a72d255286aaa401e
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-09 13:34:50 +0100

    Add files via upload

--- Abstract Classes Assignment/Employee.cs
+++ Abstract Classes Assignment/Employee.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inheritance_Assignment
+namespace Abstract_Classes_Assignment
 {
     class Employee : Person
     {
