6f27119638b2c5d6fc9efcc53398eaae3a6cc598
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-09 10:17:36 +0100

    Add files via upload

---
+++ Inheritance Assignment/Inheritance Assignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Assignment
+{
+    public class Program
+    {
+     static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.WriteLine("Type your full name, please below is example");
+
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName( employee.FirstName, employee.LastName);
+            Console.ReadLine();
+        }
+    }
+}
