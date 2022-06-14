8b7041242630f3b76a58aaa330e90124033bfaf3
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-14 20:32:41 +0100

    Add files via upload

--- Operator Overloading Assignment/Program.cs
+++ Operator Overloading Assignment/Program.cs
@@ -7,32 +7,24 @@ using System.Threading.Tasks;
 namespace Operator_Overloading_Assignment
 {
 
-    class Assignment
+    class Employee
     {
         static void Main(string[] args)
         {
-            Assignment obj1 = new Assignment();
-            Assignment obj2 = new Assignment();
-            Assignment obj3 = new Assignment();
-
 
             int ID1;
             int ID2;
-            int ID3;
 
             
                 ID1 = 0;
                 ID2 = 3;
-                ID3 = 1;
 
-                if (ID1 == ID2)
-                {
-                    Console.WriteLine("number 1 and to are equal.");
-                }
-                else if (ID1 > ID3)
-                {
-                    Console.WriteLine("number 1 are bigger than number 3.");
-                }
+
+            //summary
+            if (ID1 == ID2)
+                Console.WriteLine("error id is the same ");
+            else if (ID1 != ID2)
+                Console.WriteLine("both obejct have it diffrent ID");
 
             Console.ReadLine();
 
