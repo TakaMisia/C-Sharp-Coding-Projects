b9535b694effbab021d112276fb1ed447973c318
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-16 12:22:25 +0100

    Add files via upload

---
+++ Generics Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee<string> Names = new Employee<string>();
+
+
+            Names.Things = new List<string>()
+            {
+                "Misia",
+                "Piotr",
+                "Ewelina"
+            };
+
+
+            Employee<int> Age = new Employee<int>();
+
+            Age.Things = new List<int>()
+            {
+                24,
+                32,
+                48
+            };
+            Console.WriteLine("This is our names: ");
+            Names.Things.ForEach(Console.WriteLine);
+            Console.WriteLine("This is our age: ");
+            Age.Things.ForEach(Console.WriteLine);
+
+            Console.ReadLine();
+        }
+    }
+}
