8f5023b8ffb40cf3084f3ba9e346236654050610
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-20 08:57:16 +0100

    Add files via upload

---
+++ Structs Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Total num1;
+
+            num1.Results = 21;
+
+            Console.WriteLine(num1.Results);
+
+            Console.ReadKey();
+        }
+    }
+}
