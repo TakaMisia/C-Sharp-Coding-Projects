fe328ae3f00eb1e8ea3b6fb402f3f8bf9b0b0545
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-06-20 08:41:20 +0100

    Add files via upload

---
+++ Enums Assignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Assignment
+{
+
+    public enum Week
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thrusday,
+        Friday,
+        Saturday
+    }
+    class program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please choose any day of the week");
+            string choosen = Console.ReadLine();
+            string actual = DateTime.Now.DayOfWeek.ToString();
+            try
+            {
+                if (choosen == actual)
+                {
+                    Console.WriteLine("Your day is : " + choosen);
+                }
+                {
+                    if (choosen != actual)
+                        throw new ArgumentNullException();
+                }
+            }
+            catch (ArgumentNullException)
+
+            {
+                Console.WriteLine("Please type real day of the week");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+
