c38464ed4d77de24ff8d9f65e86f426f3fdd53ec
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-07-01 22:22:43 +0100

    Add files via upload

---
+++ DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number, please");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\00kas\Desktop\PITMAN\c# .net modules\Basic_C#_Programs\DateTime\\number.txt", true))
+            {
+                file.Write(DateTime.Now);
+                file.Write("  ");
+                file.WriteLine(number);
+
+            }
+        }
+    }
+}
