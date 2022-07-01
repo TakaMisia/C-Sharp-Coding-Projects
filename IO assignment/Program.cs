8f8c8d8b7d9904688be675204082815160f0d139
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-07-01 22:01:00 +0100

    Add files via upload

---
+++ IO assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number which i will be able to change for a text file");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\00kas\Desktop\PITMAN\c# .net modules\Basic_C#_Programs\IO assignment\\numfile.txt", true))
+            {
+                file.WriteLine(num);
+            }
+        }
+    }
+}
