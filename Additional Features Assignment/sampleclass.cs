10615c1a5dece197aaead6a979da4031cbff7868
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2022-07-11 14:09:00 +0100

    Add files via upload

---
+++ Additional Features Assignment/sampleclass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+    public class sampleclass
+    {
+        public sampleclass()
+        {
+            Console.WriteLine("2");
+        }
+        public  sampleclass(int x): this()
+        {
+            Console.Write("Misia");
+        }
+        public sampleclass(int x, string y): this(10)
+        {
+            Console.WriteLine("5");
+        }
+
+    }
+}
